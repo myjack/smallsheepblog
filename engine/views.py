e9d027713b505e12c06ded8e1a12cc454ca78c1f
ubuntu <mingyang@ubuntu.(none)>
2015-08-06 01:25:49 -0700

    edit index page and seperate html page

--- engine/views.py
+++ engine/views.py
@@ -5,7 +5,12 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
-
+#show index page
 def index(request):
     return render_to_response('index.html',{},context_instance = RequestContext(request))
-    #return HttpResponse("hello wolrd")
+
+#show sub page
+def subPage(request):
+    return render_to_response('view.html',{},context_instance = RequestContext(request))
+
+
--- smallsheepblog/urls.py
+++ smallsheepblog/urls.py
@@ -14,6 +14,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$','engine.views.index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^sub_page/$','engine.views.subPage'),	
+
+
 )
 
 if settings.DEBUG is True:
